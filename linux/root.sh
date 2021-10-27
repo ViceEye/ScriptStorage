2aba2231776db7c65e7437caebd91b61fe69fff1
ViceEye <17517159+ViceEye@users.noreply.github.com>
2021-10-27 20:12:03 +0100

    Update root.sh
    bash <(curl -Ls https://raw.githubusercontent.com/ViceEye/MyGithubStorage/main/linux/root.sh)

--- linux/root.sh
+++ linux/root.sh
@@ -1,8 +1,9 @@
+echo '输入你想要的密码'
 passwd
-q2333vv.
-q2333vv.
 
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 service sshd restart
+
+echo '成功开启root登录'
