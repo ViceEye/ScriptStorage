fbbfcce7461c816593c8f58b578bfcdf4164a71d
ViceEye <17517159+ViceEye@users.noreply.github.com>
2021-10-27 20:07:03 +0100

    Create root.sh
    For google cloud set-up

---
+++ linux/root.sh
@@ -0,0 +1,8 @@
+passwd
+q2333vv.
+q2333vv.
+
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+service sshd restart
