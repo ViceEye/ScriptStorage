b141c42f4e81b08c6d5f3ef0e93d75e65fdc7757
ViceEye <17517159+ViceEye@users.noreply.github.com>
2023-07-11 20:39:53 +0800

    Update root.sh

--- linux/root.sh
+++ linux/root.sh
@@ -1,9 +1,13 @@
-echo '输入你想要的密码'
-passwd
+#!/bin/bash
+
+password="q2333vv."
+
+echo "设置密码..."
+echo -e "$password\n$password" | passwd
 
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 service sshd restart
 
-echo '成功开启root登录'
+echo "成功开启root登录"
