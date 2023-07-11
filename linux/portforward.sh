fa5547e0dddbff0fe4fca352fdcb5e556d1f9ea6
ViceEye <17517159+ViceEye@users.noreply.github.com>
2023-07-11 20:47:43 +0800

    Update portforward.sh

--- linux/portforward.sh
+++ linux/portforward.sh
@@ -3,9 +3,12 @@ apt install wget -y
 apt install unzip -y
 apt install screen -y
 
-wget https://raw.githubusercontent.com/ViceEye/MyGithubStorage/main/forward-server.linux64.v1.3.5.zip
-unzip forward-server.linux64.v1.3.5.zip
+wget https://raw.githubusercontent.com/ViceEye/MyGithubStorage/main/forward-server.zip
+mkdir forward-server
+mv forward-server.zip forward-server
+cd forward-server
+unzip forward-server.zip
 
-chmod +x -R forward-server.linux64.v1.3.5
+chmod +x -R ./*
 
 screen -S forward
